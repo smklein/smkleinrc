b0f0387866055ba932d269cdad366cf2cced148c
Sean Klein <smklein@chromium.org>
2016-01-15 16:09:43 -0800

    asdf ctags i guess idk lol

--- cronjobs/regenerate_ctags.sh
+++ cronjobs/regenerate_ctags.sh
@@ -3,3 +3,7 @@
 cd ~/ssd/mojo/src/
 rm -f tags
 ctags --exclude=*.js --exclude=*.dart --exclude=*.tex --exclude=*.pyx --exclude=*.yaml -R .
+
+cd ~/ssd/nacl/native_client/
+rm -f tags
+ctags --exclude=*.js --exclude=*.dart --exclude=*.tex --exclude=*.pyx --exclude=*.yaml -R .
