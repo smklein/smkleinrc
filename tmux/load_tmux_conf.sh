0439f7e8ee0866a55d3300d4d8e5d481514beeaf
Sean Klein <seanmarionklein@gmail.com>
2016-05-28 19:23:19 -0700

    Dumped a prettier tmux. Reloading too!

--- tmux/load_tmux_conf.sh
+++ tmux/load_tmux_conf.sh
@@ -3,12 +3,13 @@
 load_tmux_version () {
     tmux_version="$(tmux -V | cut -c 6-)"
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
+        echo "Loading .tmux_post_2.1.conf"
         tmux source-file "${HOME}/.tmux_post_2.1.conf"
-        echo "DONE"
-        exit
+        echo "  > Done"
     else
+        echo "Loading .tmux_pre_2.1.conf"
         tmux source-file "${HOME}/.tmux_pre_2.1.conf"
-        exit
+        echo "  > Done"
     fi
 }
 
