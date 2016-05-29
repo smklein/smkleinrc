2c5d2e7327fb3a07515892c7679b5a6c2fb8e02b
Sean Klein <seanmarionklein@gmail.com>
2016-05-28 19:56:34 -0700

    WIndow renumbering, quieter reloading, vim filenames

--- tmux/load_tmux_conf.sh
+++ tmux/load_tmux_conf.sh
@@ -3,13 +3,9 @@
 load_tmux_version () {
     tmux_version="$(tmux -V | cut -c 6-)"
     if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
-        echo "Loading .tmux_post_2.1.conf"
         tmux source-file "${HOME}/.tmux_post_2.1.conf"
-        echo "  > Done"
     else
-        echo "Loading .tmux_pre_2.1.conf"
         tmux source-file "${HOME}/.tmux_pre_2.1.conf"
-        echo "  > Done"
     fi
 }
 
